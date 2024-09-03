8061b211a680ea325fe3875e20cd6d06d66ad8fc
Antoni Salvador <meigeccs@gmail.com>
2024-09-03 15:50:03 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,1 +1,22 @@
+/*Slider*/
 
+let index = 0;
+
+        function showSlide(i) {
+            const slides = document.querySelector('.slides');
+            const totalSlides = document.querySelectorAll('.slide').length;
+            index = (i + totalSlides) % totalSlides;
+            slides.style.transform = `translateX(-${index * 100}%)`;
+        }
+
+        function nextSlide() {
+            showSlide(index + 1);
+        }
+
+        function prevSlide() {
+            showSlide(index - 1);
+        }
+
+        // Automatic slide change every 5 seconds
+        setInterval(nextSlide, 5000);
+  
