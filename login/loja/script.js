786698eb939b127d171657c21b88cdbc3cd6d1a1
Antoni Salvador <meigeccs@gmail.com>
2024-09-24 17:47:19 -0300

    Update script.js

--- login/loja/script.js
+++ login/loja/script.js
@@ -1,12 +1,1 @@
-let currentSlide = 0;
 
-function moveSlide(n) {
-  const slides = document.querySelectorAll('.carousel-item');
-  currentSlide = (currentSlide + n + slides.length) % slides.length;
-  document.querySelector('.carousel-inner').style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-function closePopup() {
-    document.getElementById("whatsappPopup").style.display = "none";
-  }
-  
